1527aa4fb4fcf658f466c01a55c7a2573f75d0ca
AnaghaAjidev <anaghaajidev@gmail.com>
2026-07-13 09:55:18 +0530

    NavBar completed

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+
+        <Link className="navbar-brand" to="/">
+          Blood Donor App
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+
+            <Link className="nav-link active" to="/">
+              Add Donor
+            </Link>
+
+            <Link className="nav-link" to="/view">
+              View All Donors
+            </Link>
+
+          </div>
+        </div>
+
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
